f975c0ae56055ab25c0598f18d4dfb7637020812
KRISH-2008 <bhuvakrish25@gmail.com>
2024-04-25 14:48:38 +0530

    upadet

--- React-Project Day - 1/my-app/src/App.js
+++ React-Project Day - 1/my-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./component/Footer.jsx";
+import './App.css'
+import Content from "./component/content.jsx";
+import Header from "./component/Header.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+   return(
+    <div className="App-footer">
+      
+      <Header />
+      <Content />
+      <Footer />
     </div>
-  );
+   )
 }
 
-export default App;
+
+export default App
